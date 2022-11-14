f4c343f99455421e3dbdf9f8a67e49f24a70cb68
Rcnald <ronaldomjunior05@gmail.com>
2022-11-14 14:53:12 -0300

    notes

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const h1 = document.querySelector('h1')
+const button = document.querySelector('#openModal')
 
-	h1.onclick =  function(event){
-	console.log(event)
-	}
+const screen = document.querySelector('#nb')
+
+button.addEventListener('click', removeInvisible)
+
+document.onkeydown = function(event){
+    if(event.key === 'Escape'){
+        addInvisible()
+    }
+}
+
+function removeInvisible(){
+    screen.classList.remove('invisible')
+}
+
+function addInvisible(){
+    screen.classList.add('invisible')
+}
